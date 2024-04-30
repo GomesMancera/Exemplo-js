3c66cfd99d1454a8bc37369e3b6ada0c66d05254
GomesMancera <manceragomesbr@gmail.com>
2024-04-30 10:55:36 -0300

    Declarações, tipos, arrays e objetos.SPREAD Destructuring.

--- js/script.js
+++ js/script.js
@@ -1,43 +1,112 @@
+"use strict"
+//Hoisting
+// var nome = "Joaquim";
 
+// if(true)
+// {
+//     let nome = "João";
+// }
 
+// console.log(nome);
 
-//Declaração de variáveis
-//var - let
+// Declaração de variáveis (var, let, const):
+// Usando var
+// var x = 10;
+// console.log(x);
 
-var nome = "José";
+// // Usando let
+// let y = 20;
+// console.log(y);
 
-if(nome != ""){
-  var nome = "Jorge";
-}
+// // Usando const (constante, valor não pode ser alterado)
+// const z = 30;
+// console.log(z);
+// // z = 20;
+
+// Tipos de dados em JavaScript:
+
+// Number
+let num = 10;
+console.log(num);
+
+// String
+let str = "Olá, mundo!";
+console.log(str.charAt(0));
+console.log(str.indexOf("Olá"));
+
+// Boolean
+let bool = true;
+console.log(bool);
+
+// Array
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [6,7,8,9,10];
+console.log(arr1);
+console.table(arr1);
+// Mesclar Arrays
+let arr3 = arr1.concat(arr2);
+console.log(arr3);
+// Mesclar Arrays com SPREAD
+let arr4 = [...arr1, ...arr2];
+console.log(arr4);
+
+// Object
+let obj = { nome: "João", idade: 25, devedor: true};
+console.log(obj);
+console.table(obj);
+console.log(obj.nome);
+console.log(obj.idade);
+console.log(obj.devedor);
+console.log(obj["nome"]);
+//Quero carregar um atributo do objeto utilizando uma variável
+let atributo = "idade";
+console.log("ATRIBUTO VARIADO : " + obj[atributo]);
+
+//Adicionando um nova propriedade ao objeto OBJ e utilizando o operador SPREAD para preencher
+obj = {...obj, cidade: "São Paulo", estado: "SP"};
+console.log(obj);
+
+//DESTRUCTURING
+const pessoa = {nome : 'Maria', idade: 34, cidade: 'Florianópolis'};
+
+const {nome, idade, cidade} = pessoa;
 console.log(nome);
+console.log(idade);
+console.log(cidade);
 
 
+// EXERCÍCIOS
 
-// console.log("Olá Mundo!");
+// Conversão entre tipos de dados:
 
-// var elH1 = document.getElementById("titulo");
+// Converter string para número
+let strNumero = "10";
+let numero = parseInt(strNumero);
+console.log(numero); // Saída: 10 (número)
 
-// console.log(elH1);
+// Converter número para string
+let numeroStr = 20;
+let strNumero2 = numeroStr.toString();
+console.log(strNumero2); // Saída: "20" (string)
 
-// var elButton = document.getElementById("btn");
+// Converter para booleano
+let valor = 0;
+let booleano = Boolean(valor);
+console.log(booleano); // Saída: false (0 é considerado false)
 
-// // elButton.addEventListener("evento" , "anônima");
-// elButton.addEventListener("click", function(){
-//     var nrA1 = Math.round(Math.random());
-//     var nrA2 = Math.round(Math.random());
-//     var nrA3 = Math.round(Math.random());
-//     var nrA4 = Math.round(Math.random());
-//     var nrA5 = Math.round(Math.random());
-//     var nrA6 = Math.round(Math.random());
+// EXERCÍCIOS
 
-//     console.error("ERRO");
-//     console.info("INFO");
-//     console.warn("AVISO");
+// Exercício 1 - Declarando Variáveis:
+// Declare três variáveis diferentes (x, y, z) usando var, let e const, respectivamente. Em seguida, imprima o valor de cada variável no console.
 
-//     const arrayFrutas = ["laranja", "banana" , "maçã", "limão"];
-//     console.table(arrayFrutas);
+// Exercício 2 - Tipos de Dados:
+// Declare variáveis representando diferentes tipos de dados (número, string, booleano, array e objeto) e imprima cada uma delas no console.
 
+// Exercício 3 - Conversão de String para Número:
+// Converta a string "25" em um número e imprima no console.
 
+// Exercício 4 - Conversão de Número para String:
+// Converta o número 30 em uma string e imprima no console.
 
-//     document.body.style.backgroundColor = `#${nrA1}${nrA2}${nrA3}${nrA4}${nrA5}${nrA6}`;
-// });
+// Exercício 5 - Conversão para Booleano:
+// Converta o valor 0 para um booleano e imprima no console.
