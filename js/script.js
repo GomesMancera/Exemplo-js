86898ce7d56d23618bc2e8d1e11f72340314aa01
GomesMancera <manceragomesbr@gmail.com>
2024-04-30 10:44:07 -0300

    Exemplo de javascript sendo inicializado, creat CODEOWNERS, add codeowners, introdução JS .

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+
+
+//Declaração de variáveis
+//var - let
+
+var nome = "José";
+
+if(nome != ""){
+  var nome = "Jorge";
+}
+console.log(nome);
+
+
+
+// console.log("Olá Mundo!");
+
+// var elH1 = document.getElementById("titulo");
+
+// console.log(elH1);
+
+// var elButton = document.getElementById("btn");
+
+// // elButton.addEventListener("evento" , "anônima");
+// elButton.addEventListener("click", function(){
+//     var nrA1 = Math.round(Math.random());
+//     var nrA2 = Math.round(Math.random());
+//     var nrA3 = Math.round(Math.random());
+//     var nrA4 = Math.round(Math.random());
+//     var nrA5 = Math.round(Math.random());
+//     var nrA6 = Math.round(Math.random());
+
+//     console.error("ERRO");
+//     console.info("INFO");
+//     console.warn("AVISO");
+
+//     const arrayFrutas = ["laranja", "banana" , "maçã", "limão"];
+//     console.table(arrayFrutas);
+
+
+
+//     document.body.style.backgroundColor = `#${nrA1}${nrA2}${nrA3}${nrA4}${nrA5}${nrA6}`;
+// });
